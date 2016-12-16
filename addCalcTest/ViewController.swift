5deac4e351a785e88b8d7b82a2b63d2595642874
Ryuzi Toyama <onifinger@ryuzis-macbook-pro.local>
2016-12-17 08:30:01 +0900

    Initial Commit

---
+++ addCalcTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  addCalcTest
+//
+//  Created by Ryuzi Toyama on 12/17/16.
+//  Copyright © 2016 Ryuzi Toyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
