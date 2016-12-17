0bf76d0e5cd681e2b2debb2a91555843d204f44a
Ryuzi Toyama <onifinger@ryuzis-macbook-pro.local>
2016-12-17 11:43:41 +0900

    Add first commit
    addCalcTest app upload

--- addCalcTest/ViewController.swift
+++ addCalcTest/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var xValue: UITextField!
+    @IBOutlet weak var yValue: UITextField!
+    @IBOutlet weak var result: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func addCalc(_ sender: UIButton){
+        let value1:String? = xValue.text
+        let value2:String? = yValue.text
+        let addResult:Int? = Int(value1!)! + Int(value2!)!
+        
+        result.text = String(describing: addResult!)
+        
+    }
 
 }
 
